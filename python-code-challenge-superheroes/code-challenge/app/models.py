b8b7504c8ddca56b8115c0499beb780ea8566b7c
butch0012 <elnathanmburu@gmail.com>
2024-01-28 17:58:48 +0300

    Create an instance of the SQLAlchemy class to use with the Flask app

--- python-code-challenge-superheroes/code-challenge/app/models.py
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -1,19 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Create an instance of the SQLAlchemy class to use with the Flask app
 db = SQLAlchemy()
-
-class Hero(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    powers = db.relationship('HeroPower', backref='hero', lazy=True)
-
-class Power(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(100), nullable=False, unique=True)
-    heroes = db.relationship('HeroPower', backref='power', lazy=True)
-
-class HeroPower(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
-    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
-    strength = db.Column(db.Integer, nullable=False)
