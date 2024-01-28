a58e8764d882fd560e95e547d91de40a68170b94
butch0012 <elnathanmburu@gmail.com>
2024-01-28 17:40:24 +0300

    Get all heroes and Create a list of heroes with their id, name, and associated powers

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -2,3 +2,17 @@ from flask import Flask, jsonify, request
 from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
+
+# Set the database URI for SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database_name.db'
+db.init_app(app)
+
+# Routes
+
+# Get all heroes
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    # Create a list of heroes with their id, name, and associated powers
+    heroes_data = [{'id': hero.id, 'name': hero.name, 'powers': [hp.power.description for hp in hero.powers]} for hero in heroes]
+    return jsonify(heroes_data)
