caa0de3295c57c843d1dbc745028cceba4f43df5
butch0012 <elnathanmburu@gmail.com>
2024-01-28 18:00:36 +0300

    Define the Power model with a primary key, description, and a relationship to HeroPower

--- python-code-challenge-superheroes/code-challenge/app/models.py
+++ python-code-challenge-superheroes/code-challenge/app/models.py
@@ -8,3 +8,9 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     powers = db.relationship('HeroPower', backref='hero', lazy=True)
+
+# Define the Power model with a primary key, description, and a relationship to HeroPower
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False, unique=True)
+    heroes = db.relationship('HeroPower', backref='power', lazy=True)    
